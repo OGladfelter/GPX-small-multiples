17707308cccc006709db0d0aa508318bcada047b
ogladfelter <ogladfelter@gmail.com>
2021-12-04 17:08:40 -0600

    playing around with coloring plots by city

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function initMap(src) {
   d3.json(src).then(function(data) {
       data = data.filter(function(d) { return d.map.summary_polyline != null });
-      data = data.filter(function(d) { return d.type == "Run" });
+      //data = data.filter(function(d) { return d.type == "Run" });
 
       data.forEach(d => {
         //var path = google.maps.geometry.encoding.decodePath(d.map.summary_polyline);
@@ -15,12 +15,18 @@ function initMap(src) {
           activityCoordinates.push({lat: d[0], lng: d[1]})
         });
 
-        drawLinePlot(activityCoordinates, d.name, d.start_date);
+        var lineColor = 'black';
+        // activities starting in Chicago should be in different color
+        if (d.start_longitude > -87.815606 && d.start_longitude < -87.564981 && d.start_latitude > 41.783071 && 42.010512) {
+            lineColor = 'blue';
+        }
+
+        drawLinePlot(activityCoordinates, d.name, d.start_date, lineColor);
       });
     });
 }
 
-function drawLinePlot(data, name, date) {
+function drawLinePlot(data, name, date, lineColor) {
 
   // set the dimensions and margins of the graph
   var size = 40;
@@ -61,8 +67,8 @@ function drawLinePlot(data, name, date) {
   svg.append("path")
     .datum(data)
     .attr("fill", "none")
-    .attr("stroke", "black")
-    .attr("stroke-width", 1)
+    .attr("stroke", lineColor)
+    .attr("stroke-width", 1.5)
     .attr("d", d3.line()
         .x(function(d) { return projection([d.lng,d.lat])[0]; })
         .y(function(d) { return projection([d.lng,d.lat])[1] })
